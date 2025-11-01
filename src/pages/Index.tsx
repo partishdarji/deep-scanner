ea808b72cebd84976db9377ec186423d9ce9a8d5
Partish Darji <partishdarji@gmail.com>
2025-11-01 13:01:11 +0530

    bruuhhh

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -218,7 +218,8 @@ const Index = () => {
         <div className="container mx-auto px-4 py-4 flex items-center justify-between">
           <div className="flex items-center gap-3">
             <div className="w-10 h-10 rounded-full bg-primary/10 flex items-center justify-center glow-subtle">
-              <Shield className="w-5 h-5 text-primary" />
+              {/* <Shield className="w-5 h-5 text-primary" /> */}
+           <img src="/public/Artboard 3.png" className="w-10 h-10"  />
             </div>
             <div>
               <h1 className="font-heading text-lg font-bold">Zero Day Bot</h1>
